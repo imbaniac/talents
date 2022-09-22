552e4dd47452158e8b80aa39f66ecba085c8c9f2
Tim Reznichenko <tim.reznich@gmail.com>
2022-09-22 19:15:01 +0700

    feat: require connect to usd Dapp

--- packages/frontend/src/pages/Welcome.jsx
+++ packages/frontend/src/pages/Welcome.jsx
@@ -1,10 +1,16 @@
-import { ReactComponent as Company } from '../assets/company.svg';
+import { useAccount } from 'wagmi';
+import { useConnectModal } from '@rainbow-me/rainbowkit';
 import { useNavigate } from 'react-router-dom';
 
+import { ReactComponent as Company } from '../assets/company.svg';
+
 const Welcome = () => {
+  const { address } = useAccount();
+  const { openConnectModal } = useConnectModal();
+
   const navigate = useNavigate();
   return (
-    <div className="mt-32 flex flex-col items-center">
+    <div className="mt-16 flex flex-col items-center">
       <h1 className="text-4xl font-bold">Welcome to Talents</h1>
       <p className="text-lg mt-4 text-center">
         If you expect people to be looking for your project and your job
@@ -14,20 +20,26 @@ const Welcome = () => {
 
       <Company className="my-16 w-[200px] h-[200px]" />
 
-      <div className="flex gap-8">
-        <button
-          onClick={() => navigate('/candidates')}
-          className="btn btn-primary btn-outline"
-        >
-          Find talent
+      {!address ? (
+        <button onClick={openConnectModal} className="btn">
+          Sign in to start
         </button>
-        <button
-          onClick={() => navigate('/profile/new')}
-          className="btn btn-secondary btn-outline"
-        >
-          Find job
-        </button>
-      </div>
+      ) : (
+        <div className="flex gap-8">
+          <button
+            onClick={() => navigate('/candidates')}
+            className="btn btn-primary btn-outline"
+          >
+            Find talent
+          </button>
+          <button
+            onClick={() => navigate('/profile/new')}
+            className="btn btn-secondary btn-outline"
+          >
+            Find job
+          </button>
+        </div>
+      )}
     </div>
   );
 };
