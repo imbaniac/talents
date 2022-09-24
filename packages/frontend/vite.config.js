b5e0409bfe124f23dd8836ade4098d40a73f59aa
Tim Reznichenko <tim.reznich@gmail.com>
2022-09-24 14:53:53 +0700

    chore: delete compression, as CloudFront already do it

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -4,7 +4,6 @@ import { visualizer } from 'rollup-plugin-visualizer';
 import alias from '@rollup/plugin-alias';
 import preact from '@preact/preset-vite';
 import svgr from 'vite-plugin-svgr';
-import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
   plugins: [
@@ -18,10 +17,6 @@ export default defineConfig({
       ],
     }),
     svgr(),
-    viteCompression({
-      algorithm: 'brotliCompress',
-      deleteOriginFile: true,
-    }),
   ],
   build: {
     rollupOptions: {
